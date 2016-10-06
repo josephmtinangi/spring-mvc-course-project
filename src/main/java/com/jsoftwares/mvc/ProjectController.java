62af76bcfaee8194bcfa417522281e9bb4ef9ea7
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-06 09:39:54 +0300

    Get form parameters by using @RequestParams annotation

--- src/main/java/com/jsoftwares/mvc/ProjectController.java
+++ src/main/java/com/jsoftwares/mvc/ProjectController.java
@@ -1,6 +1,5 @@
 package com.jsoftwares.mvc;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import data.services.ProjectService;
 
@@ -39,9 +39,9 @@ public class ProjectController {
 	}
 
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	public String saveProject(HttpServletRequest request, HttpSession session) {
+	public String saveProject(@RequestParam("name") String name, HttpSession session) {
 		System.out.println(session.getAttribute("token"));
-		System.out.println(request.getParameter("name"));
+		System.out.println(name);
 		System.out.println("Invoking saveProject");
 		return "project_add";
 	}
