eebe8dee4769ff3ac915cf012741b1387a7371dd
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-07 17:53:08 +0300

    ExceptionHandler

--- src/main/java/com/jsoftwares/mvc/ResourceController.java
+++ src/main/java/com/jsoftwares/mvc/ResourceController.java
@@ -4,8 +4,11 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,34 +25,40 @@ public class ResourceController {
 	@RequestMapping("/add")
 	public String add(Model model) {
 		System.out.println("Invoking add()");
+		if (1 == 1) {
+			throw new RuntimeException("There was an error");
+		}
 		return "resource_add";
-	} 
+	}
 	
+	@ExceptionHandler(Exception.class)
+	public String handleError(HttpServletRequest request)
+	{
+		return "controller_error";
+	}
+
 	@RequestMapping("/request")
 	@ResponseBody
-	public String request(@RequestBody String resource){
+	public String request(@RequestBody String resource) {
 		System.out.println(resource);
 		// Send out an email for request
 		return "The request has been sent for approval";
 	}
-	
+
 	@ModelAttribute("resource")
-	public Resource getResource()
-	{
+	public Resource getResource() {
 		System.out.println("Adding a new resource to the model");
 		return new Resource();
 	}
-	
+
 	@ModelAttribute("typeOptions")
-	public List<String> getTypes()
-	{
+	public List<String> getTypes() {
 		return new LinkedList<>(
 				Arrays.asList(new String[] { "", "Material", "Other", "Staff", "Technical Equipment" }));
 	}
-	
+
 	@ModelAttribute("radioOptions")
-	public List<String> getRadios()
-	{
+	public List<String> getRadios() {
 		return new LinkedList<>(Arrays.asList(new String[] { "Hours", "Piece", "Tons" }));
 	}
 
@@ -57,9 +66,9 @@ public class ResourceController {
 	public List<String> getChecks() {
 		return new LinkedList<>(Arrays.asList(new String[] { "Lead Time", "Special Rate", "Requires Approval" }));
 	}
-	
+
 	@RequestMapping("/review")
-	public String review(@ModelAttribute Resource resource){
+	public String review(@ModelAttribute Resource resource) {
 		System.out.println("Invoking review()");
 		return "resource_review";
 	}
---
+++ src/main/webapp/WEB-INF/views/controller_error.jsp
@@ -0,0 +1,10 @@
+	<jsp:include page="../views/fragments/header.jsp"></jsp:include>
+	<div class="jumbotron">
+		<div class="container">
+			<p class="lead text-center">
+				An error was encountered and handled by a controller method annotated with @ExceptionHandler
+			</p>
+		</div>
+	</div>
+	<jsp:include page="../views/fragments/footer.jsp"></jsp:include>
+	
